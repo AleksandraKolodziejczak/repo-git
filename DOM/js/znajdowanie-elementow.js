2f8ce7a1bb13549a2d84b54a1b31a2f59b8a88cd
Aleksandra Kołodziejczak <aleksandra@kolodziejczak.info>
2017-10-21 16:53:18 +0200

    commit

--- DOM/js/znajdowanie-elementow.js
+++ DOM/js/znajdowanie-elementow.js
@@ -15,10 +15,19 @@ console.log(wszystkieLinki);
 var pierwszyLink = document.querySelector('.link')
 console.log(pierwszyLink);
 
-linkiPoTagu.forEach(function(tag, index) {
-    console.log(tag.outerHTML) 
-})
+//linkiPoTagu.forEach(function(tag, index) {
+//    console.log(linkiPoTagu.outerHTML) 
+//})
+//
+//linkiPoTagu.forEach(function(link, index) {
+//    console.log(tag.innerHTML)  
+//})
 
-linkiPoTagu.forEach(function(link, index) {
-    console.log(tag.innerHTML)  
-})
+var elementParent = document.getElementById( "parFirst" ).parentNode;
+console.log( elementParent );
+
+console.log(document.getElementById( "parFirst" ).children );
+console.log(document.getElementById( "parFirst" ).childNodes );
+console.log(document.getElementById( "parFirst" ).childNodes[1] );
+console.log(document.getElementById( "parFirst" ).firstChild );
+console.log(document.getElementById( "parFirst" ).lastChild );
